fa90d5114c049ace86535cf929c22ddf3b1fb301
Julia S <yulia.shtohryn@optiisolutions.com>
2023-11-20 19:42:13 +0100

    version 1.0.1

--- src/lib/features/sayHello/components/SayHello.tsx
+++ src/lib/features/sayHello/components/SayHello.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { TSayHelloProps } from "../../../types/index";
 
-const SayHello = ({ name }: TSayHelloProps) => {
+export const SayHello = ({ name }: TSayHelloProps) => {
   return <div>Hey {name}, say hello to TypeScript.</div>;
 };
-
-export default SayHello;
